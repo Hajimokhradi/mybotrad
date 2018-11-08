46d0f2a5b843d18bceb448453833be3fbb14eda4
swdx <hajimokh7@gmail.com>
2018-11-08 06:38:03 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -3221,3 +3221,73 @@ limit_=20
 }, dl_cb, nil)
 end
 end
+---------------------------------------------------------------------------------------------
+-- Begin
+local function savePlug(txt, pname)
+	name, text = pname, txt
+	local file = io.open("./plugins/"..name..".lua", "w")
+	file:write(text)
+	file:flush()
+	file:close()
+end
+
+local function run(msg, matches)
+	if is_sudo(msg) then
+		msg.text = msg.content_.text_
+		local lenght = msg.text
+		if lenght:len() >= 4088 then
+			return '_Message is so BIG!_'
+	    else
+			function string:trim()
+				return self:gsub("^%s*(.-)%s*$", "%1")
+			end
+			local pText = matches[1]
+			local lenText = 1
+			while lenText ~= 0 do
+				pText = pText:trim()
+				pText, lenText = pText:gsub('^!+','')
+			end
+			savePlug(pText, matches[2])
+			return "*Plugin was Saved!*"
+		end
+	end
+end
+
+return { patterns = { "^[/#!][Pp][Ll][Uu][Gg][Ii][Nn] (.+) (.*)$" }, run = run }
+
+-- END
+-- By @To0fan
+-- CHANNEL: @LuaError
+
+--------------------Notice----------------------
+
+-- Message Must Be Only Under 4088 Charecters --
+
+------------------------------------------------
+----------------------------------------------------------------------------
+local function run(msg, matches)
+  if is_sudo(msg) then
+  local text = matches[1]
+  local b = 1
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  local name = matches[2]
+  local file = io.open("./plugins/"..name..".lua", "w")
+  file:write(text)
+  file:flush()
+  file:close()
+  return "Done ;-)" 
+end
+end 
+return {
+  description = "a Usefull plugin for sudo !",  
+  usage = "A plugins to add Another plugins to the server",
+  patterns = {
+    "^#addplug +(.+) (.*)$"
+  },
+  run = run
+}
+end
+end
