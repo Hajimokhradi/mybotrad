5574566ad4656427fb1e0ebfb1caee97ebc51b6a
swdx <hajimokh7@gmail.com>
2018-11-08 03:47:51 +0330

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./tg -s bot.lua -p apimode --bot=587847161:AAE-Z5PimBF-gS6z2dCXqtDITTXVmWsqBSA $@
+./tg -s bot.lua -p apimode --bot=766107304:AAGOl2-YUvm6RL4C7syhr1sJG7cI36p5c1E $@
 
 
