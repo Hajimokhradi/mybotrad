0aa5900d443931298111cb356656e0fea0abea08
swdx <hajimokh7@gmail.com>
2018-11-08 03:52:15 +0330

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -12,7 +12,7 @@ rm -rf ~/.telegram-cli/$entry/data/thump/*
 rm -rf ~/.telegram-cli/$entry/data/video/*
 rm -rf ~/.telegram-cli/$entry/data/voice/*
 
-curl "https://api.telegram.org/bot587847161:AAE-Z5PimBF-gS6z2dCXqtDITTXVmWsqBSA/sendMessage" -F "chat_id=-1001374162505" -F "text=Mehdi_Yt Your Self Bot Crashed And Was Ready To Work :D"
+curl "https://api.telegram.org/bot766107304:AAGOl2-YUvm6RL4C7syhr1sJG7cI36p5c1E/sendMessage" -F "chat_id=-1001317391844" -F "text=мя.αℓιяєzα Your Self Bot Crashed And Was Ready To Work :D"
 
 sleep 8
 done
